f6e74342e2537f93280dff83e37090473af87b5e
MJDeveloping <63269417+MJDeveloping@users.noreply.github.com>
2020-10-15 14:16:30 +0100

    Part 4 Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HereosComponent } from './hereos/hereos.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HereosComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
